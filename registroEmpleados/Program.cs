906a4a850dba800535e8d44296334c3ada017173
wjruiz-debug <wjruiz@uamv.edu.ni>
2026-06-01 12:25:09 -0600

    Agregue guardado de registros .CSV

--- registroEmpleados/Program.cs
+++ registroEmpleados/Program.cs
@@ -12,15 +12,35 @@ void AgregarEmpleados(int pos)
     Console.Write("Salario: ");
     empleados[pos].salario = double.Parse(Console.ReadLine()!);
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Registro guardado satisfactoriamintente.");
+    Console.WriteLine("Registro ingresado a sistema satisfactoriamente.");
     Console.ResetColor();
 }
 
+void mostrarDatos(int pos)
+{
+    Console.WriteLine("Mostrar Registros.");
+    for (int i = 0; i< 10; i++)
+    {
+        Console.WriteLine($"Nombre: {empleados[i].nombres} \nApellidos: {empleados[i].apellidos} \nCargo: {empleados[i].cargo} \nSalario: {empleados[i].salario}");
+    }
+}
 int menu()
 {
-    Console.Write("1.Agregar \n 2.Mostrar \n 3.Eliminar \n 4.Salir. \n Digite su opcion");
+    Console.Write("1.Agregar \n 2.Mostrar \n 3.Guardar. \n 4.Salir. \n Digite su opcion");
     int op = int.Parse(Console.ReadLine()!);
     return op;
+}
+
+void guardarEmpleados(int pos)
+{
+    StreamWriter archivo = new StreamWriter("C:\\Programacion\\empleados.csv");
+    for (int i = 0; i < pos; i++)
+    {
+        archivo.WriteLine($"{empleados[i].nombres}; {empleados[i].apellidos}; {empleados[i].cargo}; {empleados[i].salario}");
+    }
+
+    archivo.Close();
+    Console.WriteLine("Registros guardados satisfactoriamente.");
 }
 
  int main()
@@ -29,12 +49,26 @@ int menu()
 
     do
     {
-        Console.Write($"Registro # {i + 1}");
+        Console.Write($"Registro # {i}");
         op = menu();
         switch (op)
         {
             case 1:
                 AgregarEmpleados(i++);
+                break;
+            case 2:
+                mostrarDatos(i);
+                break;
+            case 3:
+                guardarEmpleados(i);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Archivo guardado.");
+                Console.ResetColor();
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Opcion invalida.");
+                Console.ResetColor();
                 break;
 
                 
